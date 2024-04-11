9fef55976dd2ea61a46168d4f110ed0b2b0819f6
Criss Paolo <limcriss78@gmail.com>
2024-04-12 00:30:28 +0800

    Criss Paolo Added a news.dart

---
+++ lib/news.dart
@@ -0,0 +1,4 @@
+class NewsAPI {
+  static const String newsSentimentURL =
+      'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo'; //alphavantage free api without api key
+}
